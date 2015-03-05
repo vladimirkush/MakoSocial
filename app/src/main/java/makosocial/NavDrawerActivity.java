8c8b80c39b1ac30ac2b65d8ae772a2d0bff071cb
vladimirkush <vkushnirsky@gmail.com>
2015-03-05 13:19:51 +0200

    minor code refactor

--- app/src/main/java/makosocial/NavDrawerActivity.java
+++ app/src/main/java/makosocial/NavDrawerActivity.java
@@ -34,15 +34,6 @@ import dbObjects.MakoEvent;
 // Mike Penz lib
 
 public class NavDrawerActivity extends ActionBarActivity {
-    private final String NAME_COL = "Name";
-    private final String DESCRIPTION_COL = "Description";
-    private final String NUM_LIKES_COL = "NumLikes";
-    private final String START_DATE_COL = "StartDate";
-    private final String RATING_COL = "Rating";
-    private final String NUM_RATED_COL = "numRated";
-    private final String PICTURE_COL = "Picture";
-
-    private final String PARSE_LOGCAT_TAG = "Parse";
 
     private Drawer.Result drawer_res;
     private Toolbar toolbar;
@@ -63,11 +54,96 @@ public class NavDrawerActivity extends ActionBarActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        drawer_res = getDrawerResult();
+
+        //list view
+        lvMakoEvents = (ListView) findViewById(R.id.lvMakoEvents);
+        //set adapter
+        ListViewMakoAdapter adapter = new ListViewMakoAdapter( this);
+        lvMakoEvents.setAdapter(adapter);
+        // download entities from Parse (asynctask)
+        ArrayList<MakoEvent> mEventList = new ArrayList<MakoEvent>();
+        AsyncGetMakoEvents async = new AsyncGetMakoEvents(NavDrawerActivity.this,mEventList,adapter);
+        async.execute();
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_nav_drawer, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem share = menu.findItem(R.id.menu_action_share);
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(share);
+        setShareIntent(getShareIntent());
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+           // return true;
+        //}
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer_res.isDrawerOpen()) {
+            drawer_res.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
+
+    private Intent getShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        shareIntent.setType("text/plain");
+        return shareIntent;
+    }
+
+    private void parseInit(){
+        // test parse auth
+        // Enable Local Datastore.
+       // Parse.enableLocalDatastore(this);
+        // init Parse session
+        Parse.initialize(this, "vTOFv5b5IhCPhTrl0uqqCCXDiZSojjwt7FtzSMsU", "YAL4h7JMBz2gPClEnuQHXTyZv4R3YAnYV4Lt74JK");
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
+
+    private Drawer.Result getDrawerResult(){
         /* Using Mike Penz material design library:
         * https://github.com/mikepenz/MaterialDrawer
         */
 
-        drawer_res = new Drawer()
+        Drawer.Result dr = new Drawer()
                 .withActivity(this)
                 .withToolbar(toolbar)
                 .withActionBarDrawerToggle(true)
@@ -111,86 +187,6 @@ public class NavDrawerActivity extends ActionBarActivity {
                         }
                     }
                 }).build();
-
-        //list view
-        lvMakoEvents = (ListView) findViewById(R.id.lvMakoEvents);
-        //set adapter
-        ListViewMakoAdapter adapter = new ListViewMakoAdapter( this);
-        lvMakoEvents.setAdapter(adapter);
-        // download entities from Parse (asynctask)
-        ArrayList<MakoEvent> mEventList = new ArrayList<MakoEvent>();
-        AsyncGetMakoEvents async = new AsyncGetMakoEvents(NavDrawerActivity.this,mEventList,adapter);
-        async.execute();
-
-
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_nav_drawer, menu);
-
-        // Locate MenuItem with ShareActionProvider
-        MenuItem share = menu.findItem(R.id.menu_action_share);
-        // Fetch and store ShareActionProvider
-        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(share);
-        setShareIntent(getShareIntent());
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawer_res.isDrawerOpen()) {
-            drawer_res.closeDrawer();
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    // Call to update the share intent
-    private void setShareIntent(Intent shareIntent) {
-        if (mShareActionProvider != null) {
-            mShareActionProvider.setShareIntent(shareIntent);
-        }
-    }
-
-    private Intent getShareIntent() {
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
-        shareIntent.setType("text/plain");
-        return shareIntent;
-    }
-
-    private void parseInit(){
-        // test parse auth
-        // Enable Local Datastore.
-       // Parse.enableLocalDatastore(this);
-        // init Parse session
-        Parse.initialize(this, "vTOFv5b5IhCPhTrl0uqqCCXDiZSojjwt7FtzSMsU", "YAL4h7JMBz2gPClEnuQHXTyZv4R3YAnYV4Lt74JK");
-        ParseUser.enableAutomaticUser();
-        ParseACL defaultACL = new ParseACL();
-
-        defaultACL.setPublicReadAccess(true);
-        ParseACL.setDefaultACL(defaultACL, true);
-
+        return dr;
     }
 }
