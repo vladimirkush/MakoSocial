e336d575944d44f55b51f0913de028911222e4c6
Jenya <zsscopr@gmail.com>
2015-03-22 14:01:11 +0200

    Working on facts

--- app/src/main/java/async/AsyncGetMakoEventsFacts.java
+++ app/src/main/java/async/AsyncGetMakoEventsFacts.java
@@ -1,12 +1,21 @@
 package async;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.BaseAdapter;
 
 import java.util.ArrayList;
+
+import dbObjects.MakoEvent;
 import dbObjects.MakoEventFact;
 
 public class AsyncGetMakoEventsFacts extends AsyncTask<Void,Void,ArrayList<MakoEventFact>> {
 
+    private Context context;
+    private ArrayList<MakoEventFact> factsList;
+    private ProgressDialog progressDialog;
+    private BaseAdapter adapter;
 
     @Override
     protected ArrayList<MakoEventFact> doInBackground(Void... params) {
